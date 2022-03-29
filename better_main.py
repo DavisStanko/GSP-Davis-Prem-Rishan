389eeb6e778fc5a20f31d836244cf39f741b3608
prem <premp74@gmail.com>
2022-03-29 11:50:13 -0400

    Added a way to add themes 2.0

--- better_main.py
+++ better_main.py
@@ -2,6 +2,7 @@
 # to speech conversion
 from tkinter import *
 import tkinter.ttk as ttk
+import PIL
 from gtts import gTTS
 from deep_translator import GoogleTranslator
 import playsound
@@ -43,13 +44,51 @@ def speak():
 
 
 root = Tk()
-ttk.Style().theme_use('clam')
 
 root.geometry("700x500")
 root.title("Translate")
 root.resizable(False, False)
 
-prompt = Label(text="Enter Your Birthday:")
+#Dark/Light mode toggle
+is_on = True
+
+def switch() :
+    global is_on
+
+    # Determine is on or off
+    if is_on:
+        onButton.config(image=off)
+        is_on = False
+        root.tk.call("set_theme", "light")
+    else:
+        onButton.config(image=on)
+        is_on = True
+        root.tk.call("set_theme", "dark")
+
+
+
+# Define Our Images
+on = PhotoImage(file="dark.png")
+
+off = PhotoImage(file="light.png")
+
+onButton = Button(root, image=on, bd=0, cursor="hand2", command=switch)
+onButton.place(x=630, y=25)
+#
+
+root.tk.call("source", "azure.tcl")
+root.tk.call("set_theme", "dark")
+
+
+labelTittle = ttk.Label(root, text="Translator", font=('Helvetica', 32, 'underline'))
+labelTittle.place(x=250, y= 25)
+
+# prompt = Label(text="Enter Your Birthday:")
+
+# copy/paste https://stackoverflow.com/questions/36990396/automatically-copy-tkinter-text-widget-content-to-clipboard
+#stuff like googletrans and messagebox could be useful https://codingshiksha.com/python/python-tkinter-gui-script-to-make-language-translate-app-using-google-translate-api-full-project-for-beginners/
+#Flag stuff in options https://morioh.com/p/f900651c5e48
+
 
 entryValue = StringVar()
 option_variable = StringVar()
@@ -58,11 +97,11 @@ option_variable.set("English")
 entry = ttk.Entry(root, textvariable=entryValue, width=20)
 entry.place(x=50, y=100)
 # , width=20, bd=3, font=20, bg="#5B5750", fg="#FFF1BF"
-options = OptionMenu(root, option_variable, "English", "French", "Spanish", "Mandarin")
+options = ttk.OptionMenu(root, option_variable, "English", "French", "Spanish", "Mandarin")
 options.place(x=450, y=105)
 
-submit = Button(text="Speak", font=20, bg="#492C1D", fg="white", width=10, height=2, command=speak).place(x=325, y=450)
-
+submit = ttk.Button(text="Speak", command=speak).place(x=325, y=450)
+# , font=20, bg="#492C1D", fg="white",
 result = Label(text="", fg="white")
 result.place(x=50, y=250)
 
