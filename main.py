b7c8990348e6b70fd4eae9985f3129a45dbca24e
prem <premp74@gmail.com>
2022-03-30 14:19:06 -0400

    made placements a little better 2.1

--- main.py
+++ main.py
@@ -5,25 +5,45 @@ import tkinter.ttk as ttk
 from gtts import gTTS
 from deep_translator import GoogleTranslator
 import playsound
+import textwrap
 
 ttk.output = ""
+
+choose_langauge = {'af': 'afrikaans', 'sq': 'albanian', 'am': 'amharic', 'ar': 'arabic', 'hy': 'armenian',
+                   'az': 'azerbaijani', 'eu': 'basque', 'be': 'belarusian', 'bn': 'bengali', 'bs': 'bosnian',
+                   'bg': 'bulgarian', 'ca': 'catalan', 'ceb': 'cebuano', 'ny': 'chichewa',
+                   'zh-cn': 'chinese (simplified)', 'zh-tw': 'chinese (traditional)', 'co': 'corsican',
+                   'hr': 'croatian', 'cs': 'czech', 'da': 'danish', 'nl': 'dutch', 'en': 'english', 'eo': 'esperanto',
+                   'et': 'estonian', 'tl': 'filipino', 'fi': 'finnish', 'fr': 'french', 'fy': 'frisian',
+                   'gl': 'galician', 'ka': 'georgian', 'de': 'german', 'el': 'greek', 'gu': 'gujarati',
+                   'ht': 'haitian creole', 'ha': 'hausa', 'haw': 'hawaiian', 'iw': 'hebrew', 'hi': 'hindi',
+                   'hmn': 'hmong', 'hu': 'hungarian', 'is': 'icelandic', 'ig': 'igbo', 'id': 'indonesian',
+                   'ga': 'irish', 'it': 'italian', 'ja': 'japanese', 'jw': 'javanese', 'kn': 'kannada', 'kk': 'kazakh',
+                   'km': 'khmer', 'ko': 'korean', 'ku': 'kurdish (kurmanji)', 'ky': 'kyrgyz', 'lo': 'lao',
+                   'la': 'latin', 'lv': 'latvian', 'lt': 'lithuanian', 'lb': 'luxembourgish', 'mk': 'macedonian',
+                   'mg': 'malagasy', 'ms': 'malay', 'ml': 'malayalam', 'mt': 'maltese', 'mi': 'maori', 'mr': 'marathi',
+                   'mn': 'mongolian', 'my': 'myanmar (burmese)', 'ne': 'nepali', 'no': 'norwegian', 'ps': 'pashto',
+                   'fa': 'persian', 'pl': 'polish', 'pt': 'portuguese', 'pa': 'punjabi', 'ro': 'romanian',
+                   'ru': 'russian', 'sm': 'samoan', 'gd': 'scots gaelic', 'sr': 'serbian', 'st': 'sesotho',
+                   'sn': 'shona', 'sd': 'sindhi', 'si': 'sinhala', 'sk': 'slovak', 'sl': 'slovenian', 'so': 'somali',
+                   'es': 'spanish', 'su': 'sundanese', 'sw': 'swahili', 'sv': 'swedish', 'tg': 'tajik', 'ta': 'tamil',
+                   'te': 'telugu', 'th': 'thai', 'tr': 'turkish', 'uk': 'ukrainian', 'ur': 'urdu', 'uz': 'uzbek',
+                   'vi': 'vietnamese', 'cy': 'welsh', 'xh': 'xhosa', 'yi': 'yiddish', 'yo': 'yoruba', 'zu': 'zulu',
+                   'fil': 'Filipino', 'he': 'Hebrew'}
+
+
+def get_key(val):
+    for key, value in choose_langauge.items():
+        if val == value:
+            return key
 
 
 def lang_options():
     global output
-    option = option_variable.get()
-    if option == "English":
-        # translator
-        ttk.output = GoogleTranslator(source='auto', target="en").translate(entry.get())
-    elif option == "French":
-        # translator
-        ttk.output = GoogleTranslator(source='auto', target="fr").translate(entry.get())
-    elif option == "Spanish":
-        # translator
-        ttk.output = GoogleTranslator(source='auto', target="es").translate(entry.get())
-    elif option == "Mandarin":
-        # translator
-        ttk.output = GoogleTranslator(source='auto', target="zh-CN").translate(entry.get())
+
+    lang = get_key(option_variable.get())
+
+    ttk.output = GoogleTranslator(source="auto", target=lang).translate(entry.get(1.0, "end"))
 
 
 def speak():
@@ -44,6 +64,34 @@ def speak():
     playsound.playsound('speech.mp3', True)
 
 
+def clear():
+    entry.delete(1.0, 'end')
+    result.config(text="")
+
+
+def copy():
+    global output
+    root.clipboard_clear()
+    root.clipboard_append(result["text"])
+
+
+def switch():
+    global is_on
+    if is_on:
+        onButton.config(image=off)
+        is_on = False
+        root.tk.call("set_theme", "light")
+        copy.config(image=copyLight)
+    else:
+        onButton.config(image=on)
+        is_on = True
+        root.tk.call("set_theme", "dark")
+        copy.config(image=copyDark)
+
+
+#
+
+
 root = Tk()
 
 root.geometry("700x500")
@@ -52,42 +100,21 @@ root.resizable(False, False)
 
 # Dark/Light mode toggle
 is_on = True
-
-
-def switch():
-    global is_on
-
-    # Determine is on or off
-    if is_on:
-        onButton.config(image=off)
-        is_on = False
-        root.tk.call("set_theme", "light")
-    else:
-        onButton.config(image=on)
-        is_on = True
-        root.tk.call("set_theme", "dark")
-
-
-# Define Our Images
 on = PhotoImage(file="dark.png")
-
 off = PhotoImage(file="light.png")
 
 onButton = Button(root, image=on, bd=0, cursor="hand2", command=switch)
 onButton.place(x=630, y=25)
 #
 
+
 root.tk.call("source", "azure.tcl")
 root.tk.call("set_theme", "dark")
 
-
 labelTittle = ttk.Label(root, text="Translator", font=('Helvetica', 32, 'underline'))
-labelTittle.place(x=250, y=25)
-
-# prompt = Label(text="Enter Your Birthday:")
+labelTittle.place(x=260, y=25)
 
 # copy/paste https://stackoverflow.com/questions/36990396/automatically-copy-tkinter-text-widget-content-to-clipboard
-# stuff like googletrans and messagebox could be useful https://codingshiksha.com/python/python-tkinter-gui-script-to-make-language-translate-app-using-google-translate-api-full-project-for-beginners/
 # Flag stuff in options https://morioh.com/p/f900651c5e48
 
 
@@ -95,15 +122,31 @@ entryValue = StringVar()
 option_variable = StringVar()
 option_variable.set("English")
 
-entry = ttk.Entry(root, textvariable=entryValue, font=('Helvetica'), width=20)
-entry.place(x=50, y=100)
-# , width=20, bd=3, font=20, bg="#5B5750", fg="#FFF1BF"
-options = ttk.OptionMenu(root, option_variable, "English", "English", "French", "Spanish", "Mandarin")
-options.place(x=450, y=105)
 
-speak = ttk.Button(text="Speak", command=speak).place(x=325, y=450)
-# , font=20, bg="#492C1D", fg="white",
-result = Label(text="", fg="white")
-result.place(x=50, y=250)
+entry = Text(root, width=35, height=20, borderwidth=5, relief=RIDGE, wrap='word')
+
+entry.place(x=70, y=150)
+
+options = ttk.OptionMenu(root, option_variable, *choose_langauge.values())
+options.place(x=460, y=105)
+
+speak = ttk.Button(text="Speak", command=speak).place(x=250, y=450)
+
+result = Label(root, width=35, height=20, anchor=NW, borderwidth=5, relief=RIDGE, wraplength=300, justify=LEFT)
+
+result.place(x=375, y=153)
+
+clear = ttk.Button(root, text="Clear", cursor="hand2",
+                   command=clear)
+clear.place(x=350, y=450)
+
+copyDark = PhotoImage(file="copy-d.png")
+copyLight = PhotoImage(file="copy-l.png")
+
+copy = Button(root, image=copyDark, bd=0, cursor="hand2",
+              command=copy)
+copy.place(x=335, y=350)
+
 
 root.mainloop()
+
