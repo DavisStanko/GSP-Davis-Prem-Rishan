35bdeba3a14920c109c1d48a0a94a26ffb0dbd8f
Rishan <rishans48@gmail.com>
2022-03-30 21:25:25 -0400

    working

--- speech_recog.py
+++ speech_recog.py
@@ -1,19 +1,11 @@
 import speech_recognition as sr
 
 # https://realpython.com/python-speech-recognition/#how-speech-recognition-works-an-overview
-# https://ports.macports.org/port/flac/
 
-r = sr.Recognizer()
-mic = sr.Microphone(device_index=0)
+r = sr.Recognizer()  # set r as recognizer
+mic = sr.Microphone()  # initialize mic
 
-
-print(mic)
-
-harvard = sr.AudioFile("harvard.wav")
-with harvard as source:
-    audio = r.record(source)
+with mic as source:
+    audio = r.listen(source)  # listening for speech from mic
 
 print(r.recognize_google(audio))
-
-# print works on own computer, not sure how to test at school since we cannot use flac
-# ok lets go db
