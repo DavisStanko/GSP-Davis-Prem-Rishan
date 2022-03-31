8a0ae2ffb11f3f969c9fe1fbfb94de24665247af
Rishan <rishans48@gmail.com>
2022-03-31 12:34:29 -0400

    exception handling on languages without accents working on import switch on/off

--- module_switch.py
+++ module_switch.py
@@ -2,7 +2,7 @@ class ImportBlocker(object):
     def __init__(self, *args):
         self.module_names = args
 
-    def find_module(self, fullname, =None):
+    def find_module(self, fullname):
         try:
             import module_name
         except:
