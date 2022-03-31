31fda27bdf68b62fdb5c87e4e69b97c7f539db40
Rishan <rishans48@gmail.com>
2022-03-30 21:55:02 -0400

    made class based for easy use in main

--- speech_recog.py
+++ speech_recog.py
@@ -1,18 +1,34 @@
 import speech_recognition
 import speech_recognition as sr
 
-
 r = sr.Recognizer()  # set r as recognizer
 mic = sr.Microphone()  # initialize mic
 
-try:
-    with mic as source:
-        r.adjust_for_ambient_noise(source)  # adjusting audio based on bg noise
-        audio = r.listen(source)  # listening for speech from mic
 
-        print(r.recognize_google(audio))
-
-except speech_recognition.UnknownValueError:
-    print("Sorry, didn't understand that.")
+class ListenError(Exception):
+    """Class where you can print errors based on where called"""
 
 
+class Recognizer:
+    def __init__(self):
+        self._start_listen = None
+
+    def listen(self):
+        global recognized_speech
+
+        if self.listen() is not None:
+            raise ListenError("Recognizer not listening.")
+
+        try:
+            with mic as source:
+                r.adjust_for_ambient_noise(source)  # adjusting audio based on bg noise
+                audio = r.listen(source)  # listening for speech from mic
+
+                recognized_speech = r.recognize_google(audio)
+
+        except speech_recognition.UnknownValueError:
+            recognized_speech = "Sorry, didn't understand that."
+
+    # method for setting entry as recognized speech
+    def paste(self, entry):
+        entry = recognized_speech
